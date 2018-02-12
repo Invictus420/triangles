2917865c5bc9b633853f3e050bfb6260f9cd15b8
invictus420 <alexandar_slambert@hotmail.com>
2018-02-12 16:10:51 +0100

    sad

---
+++ src/triangle/triangleCheck.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package triangle;
+
+/**
+ *
+ * @author alexa
+ */
+public class triangleCheck {
+    
+    
+    
+    public void check(int a, int b, int c){
+    
+        if(a<= 0 || b<=0 || c<=0){
+            System.out.println("NOPE!!!");
+        }
+        else if(a==b && b==c){
+            System.out.println("Equilateral");
+        }
+        else if((a==b && b!=c)||(a==c && a!=b)||(b==c && b!=a)){
+            System.out.println("Isoceles");
+        }
+        else System.out.println("Scalene");
+        
+    }
+    
+}
